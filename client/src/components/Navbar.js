a55970f46c753c25799a766ca0f8622b359690ae
44502699 <68951572+seong-94@users.noreply.github.com>
2022-10-01 02:56:47 +0900

    home & navbar css change

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../img/logo-no-background.png";
+import logo from "../img/logo-black.png";
 import { Link } from "react-router-dom";
 
 function Navbar() {
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import "../style.scss";
+
 const posts = [
   //더미 데이터 디자인 체크를 위한
   {
@@ -15,6 +15,12 @@ const posts = [
     desc: "여기는 내용이 들어갈 위치입니다.",
     img: "https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
   },
+  {
+    id: 3,
+    title: "여기는 제목이 들어갈 위치입니다.",
+    desc: "여기는 내용이 들어갈 위치입니다.",
+    img: "https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
 ];
 function Home() {
   return (
@@ -22,14 +28,16 @@ function Home() {
       <div className="posts">
         {posts.map((post) => (
           <div className="post" key={post.id}>
-            <div>
+            <div className="img">
               <img src={post.img} alt=""></img>
             </div>
-            <Link className="link" to={`/post/${post.id}`}>
-              <h1>{post.title}</h1>
-            </Link>
-            <p>{post.desc}</p>
-            <button>Read More</button>
+            <div className="content">
+              <Link className="link" to={`/post/${post.id}`}>
+                <h1>{post.title}</h1>
+              </Link>
+              <p>{post.desc}</p>
+              <button>Read More</button>
+            </div>
           </div>
         ))}
       </div>
